73d931b384431eee17602f749d6049688813de0e
kaz-san <jerome@landriau.com>
2018-06-23 23:14:56 -0400

    Initial Commit

--- ConfMangler/Program.cs
+++ ConfMangler/Program.cs
@@ -8,8 +8,19 @@ namespace ConfMangler
 {
     class Program
     {
+
         static void Main(string[] args)
         {
+            string str_filepath = "C:\\Temp\\test.ini";
+            //Console.WriteLine(str_filepath);
+
+            string[] lines = System.IO.File.ReadAllLines(str_filepath);
+
+            lines[17] = "OpenSource=false";
+
+            System.IO.File.WriteAllLines(str_filepath, lines);
+
+            Console.ReadLine();
         }
     }
 }
