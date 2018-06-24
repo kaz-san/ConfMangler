b25d87323a79dcdbc7c9a766c8b5b2153f1fc3fe
kaz-san <jerome@landriau.com>
2018-06-23 23:32:09 -0400

    Apres commentaires du David

--- ConfMangler/Program.cs
+++ ConfMangler/Program.cs
@@ -12,11 +12,25 @@ namespace ConfMangler
         static void Main(string[] args)
         {
             string str_filepath = "C:\\Temp\\test.ini";
+            string str_currentline = "";
+            int i = 0;
             //Console.WriteLine(str_filepath);
-
+            
             string[] lines = System.IO.File.ReadAllLines(str_filepath);
 
-            lines[17] = "OpenSource=false";
+            //lines[17] = "OpenSource=false";
+
+            foreach (string line in lines)
+            {
+                str_currentline = line;
+                if (line == "OpenSource=true")
+                {
+                    lines[i] = "OpenSource=false";
+                    Console.WriteLine("Success!");
+                }
+
+                i++;
+            }
 
             System.IO.File.WriteAllLines(str_filepath, lines);
 
